585b963efee9c795a5458efc008598dc89d3f1c7
Amy Roberson <amy@roberson.xyz>
2017-01-09 14:17:37 -0500

    chapters 6 -10

---
+++ Chapter6/06_Functions.playground/Pages/Personal Meme.xcplaygroundpage/Contents.swift
@@ -0,0 +1,47 @@
+/*:
+ ## Personal Meme
+ 
+ Copy the functions you made in the previous exercise to this page if you like or write them out again for practice. Or even make a whole new set.
+ 
+ Inside one of the functions, make changes that make the whole text mean something different, or make it funnier. You could change a name to your name or a friend’s, change a word to something else that rhymes, or just play around.
+*/
+
+
+
+
+
+
+func seriously(){
+    print("Yes this is serious")
+    print("I guess this is serious")
+}
+
+func notRepeating(){
+    print("for some reason, and I'm doing something,")
+    print("this seems serious")
+}
+
+
+
+
+seriously()
+notRepeating()
+seriously()
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*:
+ _Copyright (C) 2016 Apple Inc. All Rights Reserved.\
+ See LICENSE.txt for this sample’s licensing information_
+ */
+//: [Previous](@previous)  |  page 12 of 12
